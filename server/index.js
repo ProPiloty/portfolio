eaa5390c2b97559bf7484def95e3ba186355672a
Tyler Ellingford <ellingfordtyler@gmail.com>
2019-07-15 11:46:47 -0600

    Created basic express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const {
+    SERVER_PORT
+} = process.env;
+app.use(express.static(`${__dirname}/../build`))
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port: ${SERVER_PORT}`);
+})
